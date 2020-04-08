1d9a5739dbed44202188dc1ef0942ca7247dd095
Alberto Ziliani <ziliani.alberto@outlook.it>
2020-04-08 10:34:41 +0200

    allineamento versioni precedenti

---
+++ Migrations/20200406152851_View.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+//creato migrazione customizzata con vista che ci serve
+namespace BancaTest2.Migrations
+{
+    public partial class View : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE VIEW MovimentiUtente
+AS
+SELECT CLienteId, MovimentoData, Cifra from MovimentiDare
+UNION ALL
+SELECT CLienteId, MovimentoData, Cifra from MovimentiAvere
+");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP VIEW MovimentiUtente");
+        }
+    }
+}
